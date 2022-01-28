829d41c974e492fabde3cf3c06d1df1532d2d8b3
Chiranjivee <chiranjivee960@gmail.com>
2022-01-28 08:37:02 +0530

    completed messaging service

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+
+import { MessagesRepository } from './messages.repository';
+
+@Injectable()
+export class MessagesService {
+  constructor(public messagesRepo: MessagesRepository) {}
+  findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+  findAll() {
+    return this.messagesRepo.findAll();
+  }
+  create(content: string) {
+    this.messagesRepo.create(content);
+  }
+}
